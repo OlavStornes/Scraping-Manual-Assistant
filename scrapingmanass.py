f6b723572c4a39c0d22b8449e4335986f5cfc3a5
OlavStornes <ost028@post.uit.no>
2018-10-05 02:45:23 +0200

    Integrate the two other modules

--- scrapingmanass.py
+++ scrapingmanass.py
@@ -1,5 +1,8 @@
 import sys
 import argparse
+import json
+import tablescraper as ts
+import downloader as dl
 
 
 class ArgHandler():
@@ -10,42 +13,59 @@ class ArgHandler():
 
         group = self.parser.add_mutually_exclusive_group()
         group.add_argument(
-            "-d", "--download",
-            help="Download the manuals for one or multiple systems",
-            nargs='+',
-            type=int
-        )
-        group.add_argument(
             "-l", "--list",
             help="Lists all available systems and their number code",
             action="store_true"
         )
         group.add_argument(
+            "-d", "--download",
+            help="Download the manuals for one or multiple systems",
+            nargs='+',
+            type=str
+        )
+        group.add_argument(
             "-s", "--scrapedatabase",
             help="Download the database for one or multiple systems",
             nargs='+',
-            type=int
+            type=str
         )
         group.add_argument(
             "-b", "--both",
             help="Download both the database and scrape the \
                     manual for one or multiple systems",
             nargs='+',
-            type=int
+            type=str
         )
 
+    def load_data(self):
+        with open('systems.json') as f:
+            self.data = json.load(f)
+
     def showlist(self):
-        with open('systems.csv') as f:
-            for index, line in enumerate(f):
-                print(str(index) + ": \t" + line.replace(',', ''), end='')
+        self.load_data()
+
+        for line in self.data:
+            print(line + "\t" + self.data[line])
 
     def downloadmanual(self, args):
-        raise NotImplementedError
+        self.load_data()
+
+        for x in args:
+            system = self.data[x]
+            mandownloader = dl.Manualdownloader(system)
+            mandownloader.main()
 
     def scrapedatabase(self, args):
-        raise NotImplementedError
+        self.load_data()
 
-    def scrape_download(self, args):
+        for x in args:
+            system = self.data[x]
+            scraper = ts.Scrapie(system)
+            scraper.run()
+
+    def scrape_and_download(self, args):
+        self.load_data()
+
 
         self.downloadmanual()
         self.scrapedatabase()
@@ -64,15 +84,15 @@ class ArgHandler():
 
         elif (args.scrapedatabase):
             # Activate tablescraper.py
-            self.scrapedatabase(args.path)
+            self.scrapedatabase(args.scrapedatabase)
 
         elif (args.both):
             # Do both
-            self.scrape_download(args.path)
+            self.scrape_and_download()
 
         # Avoid starting a nonexistent handler
         else:
-            argparse.print_help()
+            self.parser.print_help()
             sys.exit(2)
 
 
